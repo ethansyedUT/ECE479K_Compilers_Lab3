6cc11ccec314974b873962f8ad3a3d5f092e8b97
ethansyedUT <ethansyed@utexas.edu>
2024-04-23 18:31:21 -0500

    Setup

---
+++ handout/build/CMakeFiles/Lab3.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/code/RegisterPasses.cpp" "CMakeFiles/Lab3.dir/RegisterPasses.cpp.o" "gcc" "CMakeFiles/Lab3.dir/RegisterPasses.cpp.o.d"
+  "/code/UnitLICM.cpp" "CMakeFiles/Lab3.dir/UnitLICM.cpp.o" "gcc" "CMakeFiles/Lab3.dir/UnitLICM.cpp.o.d"
+  "/code/UnitLoopInfo.cpp" "CMakeFiles/Lab3.dir/UnitLoopInfo.cpp.o" "gcc" "CMakeFiles/Lab3.dir/UnitLoopInfo.cpp.o.d"
+  "/code/UnitSCCP.cpp" "CMakeFiles/Lab3.dir/UnitSCCP.cpp.o" "gcc" "CMakeFiles/Lab3.dir/UnitSCCP.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ handout/build/CMakeFiles/Lab3.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab3.dir/RegisterPasses.cpp.o"
+  "CMakeFiles/Lab3.dir/RegisterPasses.cpp.o.d"
+  "CMakeFiles/Lab3.dir/UnitLICM.cpp.o"
+  "CMakeFiles/Lab3.dir/UnitLICM.cpp.o.d"
+  "CMakeFiles/Lab3.dir/UnitLoopInfo.cpp.o"
+  "CMakeFiles/Lab3.dir/UnitLoopInfo.cpp.o.d"
+  "CMakeFiles/Lab3.dir/UnitSCCP.cpp.o"
+  "CMakeFiles/Lab3.dir/UnitSCCP.cpp.o.d"
+  "libLab3.pdb"
+  "libLab3.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
